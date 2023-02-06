f58a4be97ec567566e585b23b6369dbe1e2b9dfe
Inssg <nightwalker0131@naver.com>
2023-02-06 18:33:32 +0900

    test: RestAssured 적용한 API 테스트로 변경

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -1,19 +1,37 @@
 package com.example.productorderservice.product;
 
+import com.example.productorderservice.ApiTest;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
-public class ProductApiTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class ProductApiTest extends ApiTest {
+
     @Autowired
     private ProductService productService;
-
 
     @Test
     void 상품등록() {
         final AddProductRequest request = 상품등록요청_생성();
+
+        //요청을 보내는 로그를 남긴다
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         productService.addProduct(request);
     }
