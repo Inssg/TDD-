63b0e5122558266609d90749f6228fd41fcd6eea
Inssg <nightwalker0131@naver.com>
2023-02-06 16:51:08 +0900

    test:상품등록 -Assert 추가

--- src/test/java/com/example/productorderservice/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.productorderservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 public class ProductServiceTest {
 
@@ -14,9 +15,18 @@ public class ProductServiceTest {
         productService.addProduct(request);
     }
 
-    private class AddProductRequest{
-        public AddProductRequest(final String name, final int price) {
-            throw new UnsupportedOperationException("Unspported AddProductRequest");
+    private static class AddProductRequest{
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        public AddProductRequest(final String name, final int price,DiscountPolicy discountPolicy) {
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy,"할인 정책은 필수입니다.");
+            this.name = name;
+            this.price =price;
+            this.discountPolicy = discountPolicy;
         }
     }
     private enum DiscountPolicy{
