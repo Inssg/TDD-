6d53b337ba89103b4d9875d6569908ada27a7966
Inssg <nightwalker0131@naver.com>
2023-02-06 16:40:26 +0900

    test:상품등록- AddProductRequest 이너클리스 생성

--- src/test/java/com/example/productorderservice/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/ProductServiceTest.java
@@ -7,7 +7,18 @@ public class ProductServiceTest {
 
     @Test
     상품등록() {
+        final String name = "상품명";
+        final int price = 1000;
+      final  AddProductRequest request = new AddProductRequest("상품명", 1000);
         productService.addProduct(request);
     }
+
+    private class AddProductRequst{
+        public AddProductRequst(final String name, final int price) {
+            throw new UnsupportedOperationException("Unspported AddProductRequest");
+        }
+    }
+
+
 
 }
