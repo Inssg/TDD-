8df1fc697803d527e1adb92f72e705b2004abdf3
Inssg <nightwalker0131@naver.com>
2023-02-06 16:35:51 +0900

    상품등록 test추가

---
+++ src/test/java/com/example/productorderservice/ProductServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.productorderservice;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+
+    @Test
+    상품등록() {
+        productService.addProduct(request);
+    }
+
+}
