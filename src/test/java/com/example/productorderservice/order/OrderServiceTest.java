9da6ac4e278ba139a27c98c6e5ae0a2dd9054599
Inssg <nightwalker0131@naver.com>
2023-02-11 19:20:10 +0900

    refac: OrderService,OrderRepsitory,OrderAdapter 스프링빈에 등록될 수있게@component 추가 & 주문 테스트 Setup 삭제 및 SpringBootTest로 변경

--- src/test/java/com/example/productorderservice/order/OrderServiceTest.java
+++ src/test/java/com/example/productorderservice/order/OrderServiceTest.java
@@ -4,32 +4,13 @@ import com.example.productorderservice.product.DiscountPolicy;
 import com.example.productorderservice.product.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class OrderServiceTest {
-
+    @Autowired
     private OrderService orderService;
-    private OrderPort orderPort;
-    private OrderRepository orderRepository;
-
-
-    @BeforeEach
-    void setUp() {
-        orderRepository = new OrderRepository();
-        //stub
-        orderPort = new OrderPort() {
-            @Override
-            public Product getProductById(Long productId) {
-                return new Product("상품명", 1000, DiscountPolicy.NONE);
-            }
-
-            @Override
-            public void save(Order order) {
-                orderRepository.save(order);
-            }
-        };
-
-        orderService = new OrderService(orderPort);
-    }
 
     @Test
     void 상품주문() {
